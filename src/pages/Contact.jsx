80771f4ce394832a77a39d8c1c46bba2d23b02e7
Vik Boyechko <vik.boyechko@gmail.com>
2024-03-31 18:36:35 -0500

    add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,34 @@
+import Header from '../components/Header';
+
 export default function Contact() {
     return (
-        <div>
-            <h1>Contact Page</h1>
-            <p>
-                Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-                Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-                mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-                lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-                in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-            </p>
-        </div>
+        <>
+            <Header>Contact</Header>
+            {/* <div className='w-25'></div> */}
+            <section className='mx-auto'>
+                <div className='p-5'>
+                    <h2>Get in Touch</h2>
+                    <p>Feel free to reach out to me with any questions or opportunities.</p>
+                    <form>
+                        <div className='form-group'>
+                            <label for='name'>Name</label>
+                            <input type='text' className='form-control' id='name' />
+                        </div>
+                        <div className='form-group mt-3'>
+                            <label for='email'>Email address</label>
+                            <input type='email' className='form-control' id='email'></input>
+                        </div>
+                        <div className='form-group mt-3'>
+                            <label for='message'>Message</label>
+                            <textarea className='form-control' id='message' rows='3'></textarea>
+                        </div>
+                        <button type='submit' className='btn btn-secondary mt-4'>Submit</button>
+                    </form>
+                </div>
+            </section>
+            {/* <div className='w-25'></div> */}
+
+
+        </>
     );
 }
